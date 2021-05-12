479c5de16aaa01159f0874a0d21dc02a5bd92f4c
Karan Sanjeev <yo@alphaman.me>
2021-05-12 14:16:01 +0530

    Bootstrap the whole project

--- src/Index.ts
+++ src/Index.ts
@@ -1,1 +1,35 @@
-import MCQParser from './Parser/MCQParser'
+import { parametreize } from './Helpers'
+import * as writeJsonFile from 'write-json-file'
+import MCQParser, { MCQ } from './Parser/MCQParser'
+import SubjectParser, { Topic } from './Parser/SubjectParser'
+import DegreeParser, { Subject } from './Parser/DegreeParser'
+
+const degreeName = 'Computer Science'
+const degreeLink = 'https://www.sanfoundry.com/computer-science-questions-answers/'
+
+const writeJson = async (degreeName: string, subject: Subject, topic: Topic, mcqs: MCQ[]) => {
+    const p = parametreize
+    const fileName = p(topic.name) + '.json'
+    const filePath = `data/${p(degreeName)}/${p(subject.name)}/${fileName}`
+
+    return await writeJsonFile(filePath, mcqs)
+}
+
+const handler = async () => {
+    const degreeParser = await DegreeParser(degreeName, degreeLink)
+    degreeParser.subjects.forEach(async (subjects) => {
+        const subjectName = subjects.name
+        const subjectParser = await SubjectParser(subjectName, subjects.link)
+
+        subjectParser.topics.forEach(async (topics) => {
+            const topicName = topics.name
+            const mcqParser = await MCQParser(topicName, topics.link)
+
+            const writer = writeJson(degreeName, subjects, topics, mcqParser.mcqs)
+            writer.then(() => console.log('Scrapped', degreeName, subjectName, topicName))
+                .catch(() => console.error('Couldn\'t Scrape', degreeName, subjectName, topicName))
+        })
+    })
+}
+
+handler()
