8007241357e82c2c7c274970814da6b8e254d877
Karan Sanjeev <yo@alphaman.me>
2021-05-14 14:54:02 +0530

    Completed rewrite of MCQParser into golang

--- MCQParser.go
+++ MCQParser.go
@@ -12,18 +12,64 @@ type Answer struct {
 	Explanation string
 }
 
-type Option struct {
-	Text string
-}
-
 type MCQ struct {
 	Question string
-	options  []Option
-	answer   []Answer
+	Options  []string
+	Answer
 }
 
-func MCQParser(link string) {
+type ParserResult struct {
+	Topic string
+	MCQs  []MCQ
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if strings.EqualFold(v, str) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func optionsParser(rawText []string) (options []string, exists bool) {
+	optionStarters := []string{"a)", "b)", "c)", "d)"}
+
+	for _, line := range rawText {
+		for _, starter := range optionStarters {
+			if !strings.HasPrefix(line, starter) {
+				continue
+			}
+
+			options = append(options, strings.ReplaceAll(line, starter+" ", ""))
+		}
+	}
+
+	if len(options) > 0 {
+		exists = true
+	}
+
+	return
+}
+
+func mcqBuilder(answers []Answer, options [][]string, questions []string) (mcqs []MCQ) {
+	for i, question := range questions {
+		mcqs = append(mcqs, MCQ{
+			Question: question,
+			Options:  options[i],
+			Answer:   answers[i],
+		})
+	}
+
+	return
+}
+
+func MCQParser(topic string, link string) (mcqs ParserResult, err error) {
 	var answers []Answer
+	var options [][]string
+	var questions []string
+
 	c := colly.NewCollector()
 
 	c.OnError(func(_ *colly.Response, err error) {
@@ -42,15 +88,47 @@ func MCQParser(link string) {
 				Explanation: strings.ReplaceAll(answerRaw[1], "Explanation: ", ""),
 			})
 		})
+
+		e.ForEach("div.entry-content p", func(i int, element *colly.HTMLElement) {
+			if i == 0 {
+				return
+			}
+
+			questionsRaw := strings.Split(element.Text, "\n")
+			if val, exists := optionsParser(questionsRaw); exists {
+				options = append(options, val)
+			}
+
+			questionNotFound := true
+			for questionNotFound {
+				if contains(questionsRaw, "View Answer") {
+					if !strings.HasPrefix(questionsRaw[0], "a)") {
+						questions = append(questions, questionsRaw[0])
+						questionNotFound = false
+					}
+				}
+
+				if len(questionsRaw) == 1 {
+					questions = append(questions, questionsRaw[0])
+					questionNotFound = false
+				}
+
+				questionNotFound = false
+			}
+		})
 	})
 
 	c.OnScraped(func(r *colly.Response) {
 		fmt.Println("Finished", r.Request.URL)
 	})
 
-	if err := c.Visit(link); err != nil {
-		fmt.Println("Couldn't visit ", link)
+	err = c.Visit(link)
+
+	questions = questions[:len(questions)-2]
+	mcqs = ParserResult{
+		Topic: topic,
+		MCQs:  mcqBuilder(answers, options, questions),
 	}
 
-	fmt.Println(answers)
+	return
 }
