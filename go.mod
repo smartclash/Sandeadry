cd1e7b3c679e972318353c7de8cbcb5c315d1a46
Karan Sanjeev <yo@alphaman.me>
2022-05-25 08:18:15 +0530

    MCQ Parser done

--- go.mod
+++ go.mod
@@ -1,22 +1,26 @@
 module github.com/smartclash/Sandeadry
 
-go 1.16
+go 1.18
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.1
+	github.com/gocolly/colly v1.2.0
+	github.com/samber/lo v1.21.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.1.0 // indirect
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.3.6 // indirect
+	github.com/antchfx/xpath v1.1.10 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0
-	github.com/joho/godotenv v1.3.0
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/meilisearch/meilisearch-go v0.14.1
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
-	github.com/thatisuday/commando v1.0.4
-	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc // indirect
+	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	gorm.io/driver/sqlite v1.1.4
-	gorm.io/gorm v1.21.9
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
+	"github.com/samber/lo"
 	"net/url"
 	"strings"
 	"time"
@@ -12,8 +13,8 @@ import (
 
 var c = colly.NewCollector(
 	colly.AllowedDomains("sanfoundry.com", "www.sanfoundry.com"),
-	colly.AllowURLRevisit(),
-	colly.Async(true))
+	colly.Async(true),
+	colly.CacheDir("./sanfoundry_cache"))
 
 type Subject struct {
 	Name string
@@ -25,6 +26,18 @@ type Topic struct {
 	Link string
 }
 
+type Answer struct {
+	Answer      string
+	Explanation string
+}
+
+type MCQ struct {
+	Question string
+	Options  []string
+
+	Answer
+}
+
 func main() {
 	var theURL string
 
@@ -33,8 +46,9 @@ func main() {
 	flag.Parse()
 	c.SetRequestTimeout(time.Minute * 2)
 
-	subRes := make(chan Subject, 10)
-	topicRes := make(chan Topic, 1000)
+	subRes := make(chan Subject)
+	topicRes := make(chan Topic)
+	mcqRes := make(chan []MCQ)
 
 	go degreeParser(theURL, subRes)
 
@@ -43,7 +57,11 @@ func main() {
 	}
 
 	for topic := range topicRes {
-		fmt.Println("Topic parsed", topic.Name)
+		go topicParser(topic, mcqRes)
+	}
+
+	for mcqs := range mcqRes {
+		fmt.Println(mcqs)
 	}
 
 	c.Wait()
@@ -74,7 +92,7 @@ func degreeParser(theURL string, subRes chan<- Subject) {
 	})
 
 	c.OnHTML("title", func(element *colly.HTMLElement) {
-		fmt.Println(strings.ReplaceAll(element.Text, " Questions and Answers - Sanfoundry", ""))
+		fmt.Println("Scrapped degree", element.Text)
 	})
 
 	if err := c.Visit(theURL); err != nil {
@@ -94,7 +112,81 @@ func subjectParser(sub Subject, topicRes chan<- Topic) {
 		})
 	})
 
+	c.OnHTML("title", func(element *colly.HTMLElement) {
+		fmt.Println("Scrapped subject", element.Text, sub.Link)
+	})
+
 	if err := c.Visit(sub.Link); err != nil {
+		fmt.Println("Couldn't visit the website", err.Error())
+	}
+}
+
+func topicParser(topic Topic, mcqRes chan<- []MCQ) {
+	fmt.Println("inside topic parser", topic.Name)
+	offset := 0
+	var mcqs []MCQ
+	var answers []Answer
+	var skippedQuestions []int
+
+	c.OnHTML("div.entry-content", func(e *colly.HTMLElement) {
+		e.ForEach("div.collapseomatic_content", func(_ int, ans *colly.HTMLElement) {
+			answerRaw := strings.Split(ans.Text, "\n")
+			answer := strings.ReplaceAll(answerRaw[0], "Answer: ", "")
+			explanation := strings.ReplaceAll(answerRaw[1], "Explanation: ", "")
+
+			answers = append(answers, Answer{
+				Answer:      answer,
+				Explanation: explanation,
+			})
+		})
+
+		e.ForEach("div.entry-content p", func(i int, element *colly.HTMLElement) {
+			if !strings.Contains(element.Text, "View Answer") {
+				offset++
+				return
+			}
+
+			theText := strings.Split(strings.ReplaceAll(element.Text, "View Answer", ""), "\n")
+			if len(theText) <= 5 {
+				skippedQuestions = append(skippedQuestions, i-offset)
+				return
+			}
+
+			theText = lo.Reject(theText, func(line string, _ int) bool {
+				return line == ""
+			})
+
+			mcq := MCQ{}
+			starters := []string{"a)", "b)", "c)", "d)"}
+			for _, line := range theText {
+				if lo.Contains(starters, line[0:2]) {
+					mcq.Options = append(mcq.Options, line)
+				} else {
+					mcq.Question = mcq.Question + "\n" + line
+				}
+			}
+
+			mcqs = append(mcqs, mcq)
+		})
+
+		// Construct the MCQ with collected data
+		count := 0
+		for index, answer := range answers {
+			questionNumber := index + 1
+			if !lo.Contains(skippedQuestions, questionNumber) {
+				mcqs[count].Answer = answer
+				count++
+			}
+		}
+
+		mcqRes <- mcqs
+	})
+
+	c.OnHTML("title", func(element *colly.HTMLElement) {
+		fmt.Println("Scrapped MCQ", element.Text)
+	})
+
+	if err := c.Visit(topic.Link); err != nil {
 		fmt.Println("Couldn't visit the website", err.Error())
 	}
 }
