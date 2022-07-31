99b6b2a1ffca4c55f84d9090e9cb7c97c5fadcb7
Karan Sanjeev <yo@alphaman.me>
2022-08-01 00:21:03 +0530

    After hours of work, a working recode

--- go.mod
+++ go.mod
@@ -2,25 +2,33 @@ module github.com/smartclash/Sandeadry
 
 go 1.18
 
-require (
-	github.com/PuerkitoBio/goquery v1.6.1
-	github.com/gocolly/colly v1.2.0
-	github.com/samber/lo v1.21.0
-)
+require github.com/geziyor/geziyor v0.0.0-20220429000531-738852f9321d
 
 require (
-	github.com/andybalholm/cascadia v1.1.0 // indirect
-	github.com/antchfx/htmlquery v1.2.3 // indirect
-	github.com/antchfx/xmlquery v1.3.6 // indirect
-	github.com/antchfx/xpath v1.1.10 // indirect
-	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
-	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/VividCortex/gohistogram v1.0.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/chromedp/cdproto v0.0.0-20220428002153-285dfb42699c // indirect
+	github.com/chromedp/chromedp v0.8.0 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/go-kit/kit v0.12.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.12.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.34.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
-	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
-	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc // indirect
-	golang.org/x/text v0.3.2 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,241 +1,99 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
-	"net/url"
-	"os"
-	"strings"
-	"time"
-
 	"github.com/PuerkitoBio/goquery"
-	"github.com/gocolly/colly"
-	"github.com/samber/lo"
+	"github.com/geziyor/geziyor"
+	"github.com/geziyor/geziyor/client"
+	"github.com/geziyor/geziyor/export"
+	"net/url"
+	"strings"
 )
 
-var c = colly.NewCollector(
-	colly.AllowedDomains("sanfoundry.com", "www.sanfoundry.com"),
-	colly.Async(true),
-	colly.CacheDir("./sanfoundry_cache"),
-)
-
-type Subject struct {
-	Name       string
-	Link       string
-	DegreeName string
+func main() {
+	geziyor.NewGeziyor(&geziyor.Options{
+		StartURLs:          []string{"https://www.sanfoundry.com/computer-science-questions-answers/"},
+		ParseFunc:          subjectParse,
+		Exporters:          []export.Exporter{&export.JSON{}},
+		ConcurrentRequests: 10,
+	}).Start()
 }
 
-type Topic struct {
-	Name        string
-	Link        string
-	SubjectName string
+func stringCleaner(text string) string {
+	replacers := []string{
+		" MCQs with Answers - Sanfoundry",
+		" MCQs - Sanfoundry",
+		"50000+ ",
+	}
+
+	for _, replacer := range replacers {
+		text = strings.ReplaceAll(text, replacer, "")
+	}
+
+	return text
 }
 
-type Answer struct {
-	Answer      string
-	Explanation string
+func subjectParse(g *geziyor.Geziyor, r *client.Response) {
+	r.HTMLDoc.Find("table tbody tr td li a").Each(func(i int, s *goquery.Selection) {
+		href, exists := s.Attr("href")
+		if !exists {
+			return
+		}
+
+		link, _ := url.Parse(href)
+		if link.Host == "rank.sanfoundry.com" {
+			return
+		}
+
+		rawTitle := r.HTMLDoc.Find("title").Text()
+		title := stringCleaner(rawTitle)
+
+		req, _ := client.NewRequest("GET", href, nil)
+		req.Meta["subject"] = title
+
+		g.Do(req, topicParse)
+	})
+}
+
+func topicParse(g *geziyor.Geziyor, r *client.Response) {
+	r.HTMLDoc.Find("table tbody tr td li a").Each(func(i int, s *goquery.Selection) {
+		href, _ := s.Attr("href")
+		subject := fmt.Sprint(r.Request.Meta["subject"])
+
+		req, _ := client.NewRequest("GET", href, nil)
+		req.Meta["subject"] = subject
+		req.Meta["topic"] = s.Text()
+
+		g.Do(req, mcqParse)
+	})
 }
 
 type MCQ struct {
-	Question string
-	Options  []string
-
-	Answer
-	TopicName string
+	Subject string
+	Topic   string
+	Result  []interface{}
 }
 
-func main() {
-	go func() { log.Fatal(http.ListenAndServe(":4000", nil)) }()
+func mcqParse(g *geziyor.Geziyor, r *client.Response) {
+	var answers []interface{}
+	dom := r.HTMLDoc.Find("div.entry-content")
 
-	var theURL string
+	dom.Find("div.collapseomatic_content").Each(func(i int, s *goquery.Selection) {
+		answerRaw := strings.Split(s.Text(), "\n")
+		answer := strings.ReplaceAll(answerRaw[0], "Answer: ", "")
+		explanation := strings.ReplaceAll(answerRaw[1], "Explanation: ", "")
 
-	flag.StringVar(&theURL, "u", "", "Specify the degree url to parse")
-
-	flag.Parse()
-	c.SetRequestTimeout(time.Minute * 2)
-
-	if len(theURL) <= 0 {
-		fmt.Println("Please enter a degree link")
-		return
-	}
-
-	subRes := make(chan Subject, 10)
-	topicRes := make(chan Topic, 10)
-	mcqRes := make(chan []MCQ, 10)
-
-	go degreeParser(theURL, subRes)
-	go subjectWorker(subRes, topicRes)
-	go topicWorker(topicRes, mcqRes)
-
-	file, err := os.Create("lmao/mcq.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-
-	tf, err := os.OpenFile("lmao/mcq.json", os.O_APPEND|os.O_WRONLY, 0777)
-	if err != nil {
-		fmt.Print("cry")
-	}
-	defer tf.Close()
-
-	for mcqs := range mcqRes {
-		res, err := json.Marshal(mcqs)
-		if err != nil {
-			fmt.Println("bruh")
+		finalAnswer := map[string]string{
+			"Answer":      answer,
+			"Explanation": explanation,
 		}
 
-		tf.Write(res)
-	}
-
-	c.Wait()
-}
-
-func subjectWorker(subRes chan Subject, topicRes chan Topic) {
-	for sub := range subRes {
-		go subjectParser(sub, topicRes)
-	}
-}
-
-func topicWorker(topicRes chan Topic, mcqRes chan []MCQ) {
-	for topic := range topicRes {
-		go topicParser(topic, mcqRes)
-	}
-}
-
-func degreeParser(theURL string, subRes chan<- Subject) {
-	c.OnHTML("li", func(e *colly.HTMLElement) {
-		e.DOM.Find("div.entry-content table tbody tr td li a").Each(func(_ int, selection *goquery.Selection) {
-			href, exists := selection.Attr("href")
-			if !exists {
-				return
-			}
-
-			link, err := url.Parse(href)
-			if err != nil {
-				return
-			}
-
-			if strings.EqualFold(link.Host, "rank.sanfoundry.com") {
-				return
-			}
-
-			subRes <- Subject{
-				//DegreeName:
-				Name: selection.Text(),
-				Link: href,
-			}
-		})
+		answers = append(answers, finalAnswer)
 	})
 
-	c.OnHTML("title", func(element *colly.HTMLElement) {
-		//fmt.Println("Scrapped subject", element.Text)
-	})
-
-	if err := c.Visit(theURL); err != nil {
-		//fmt.Println("Couldn't visit degree site", err.Error())
-	}
-}
-
-func subjectParser(sub Subject, topicRes chan<- Topic) {
-	c.OnHTML("div.sf-section", func(e *colly.HTMLElement) {
-		e.ForEach("div.sf-section table tbody tr td li a", func(bruh int, selection *colly.HTMLElement) {
-			href := selection.Attr("href")
-			topic := Topic{
-				Name: selection.Text,
-				Link: href,
-			}
-
-			topicRes <- topic
-		})
-	})
-
-	c.OnScraped(func(res *colly.Response) {
-		//fmt.Println("Scrapped topic", sub.Link)
-	})
-
-	if err := c.Visit(sub.Link); err != nil {
-		//fmt.Println("Couldn't visit subject site", err.Error())
-	}
-}
-
-func topicParser(topic Topic, mcqRes chan<- []MCQ) {
-	offset := 0
-	var mcqs []MCQ
-	var answers []Answer
-	var skippedQuestions []int
-
-	c.OnHTML("div.entry-content", func(e *colly.HTMLElement) {
-		e.ForEach("div.collapseomatic_content", func(_ int, ans *colly.HTMLElement) {
-			if len(answers) <= 0 {
-				return
-			}
-
-			answerRaw := strings.Split(ans.Text, "\n")
-			answer := strings.ReplaceAll(answerRaw[0], "Answer: ", "")
-			explanation := strings.ReplaceAll(answerRaw[1], "Explanation: ", "")
-
-			answers = append(answers, Answer{
-				Answer:      answer,
-				Explanation: explanation,
-			})
-		})
-
-		e.ForEach("div.entry-content p", func(i int, element *colly.HTMLElement) {
-			if !strings.Contains(element.Text, "View Answer") {
-				offset++
-				return
-			}
-
-			theText := strings.Split(strings.ReplaceAll(element.Text, "View Answer", ""), "\n")
-			if len(theText) <= 5 {
-				skippedQuestions = append(skippedQuestions, i-offset)
-				return
-			}
-
-			theText = lo.Reject(theText, func(line string, _ int) bool {
-				return line == ""
-			})
-
-			mcq := MCQ{}
-			starters := []string{"a)", "b)", "c)", "d)"}
-			for _, line := range theText {
-				if len(line) <= 1 {
-					return
-				}
-
-				if lo.Contains(starters, line[0:2]) {
-					mcq.Options = append(mcq.Options, line)
-				} else {
-					mcq.Question = mcq.Question + "\n" + line
-				}
-			}
-
-			mcqs = append(mcqs, mcq)
-		})
-
-		// Construct the MCQ with collected data
-		count := 0
-		for index, answer := range answers {
-			questionNumber := index + 1
-			if !lo.Contains(skippedQuestions, questionNumber) {
-				mcqs[count].Answer = answer
-				count++
-			}
-		}
-
-		mcqRes <- mcqs
-	})
-
-	c.OnHTML("title", func(element *colly.HTMLElement) {
-		//fmt.Println("Scrapped MCQ", element.Text)
-	})
-
-	if err := c.Visit(topic.Link); err != nil {
-		//fmt.Println("Couldn't visit topic site", err.Error())
+	g.Exports <- MCQ{
+		Subject: fmt.Sprint(r.Request.Meta["subject"]),
+		Topic:   fmt.Sprint(r.Request.Meta["topic"]),
+		Result:  answers,
 	}
 }
